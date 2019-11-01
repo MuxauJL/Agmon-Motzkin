20ae9d2223d14980ee2cfd33e8054d099e3422b9
Mikhail Viryasov <viryasov.mikhail@gmail.com>
2019-11-02 00:23:55 +0300

    Clp was integrated

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "Agmon_Motzkin.h"
 #include "MPS_Parser.h"
-#include <omp.h>
+#include "ClpSimplex.hpp"
 
 double get_distance(const std::vector<double>& v1, const std::vector<double>& v2) {
 	if (v1.size() != v2.size())
@@ -112,93 +112,115 @@ int main() {
 	//	std::cout << "\n";
 	//}
 
-	//omp_set_num_threads(4);
 	MPS_Parser parser;
 	std::unique_ptr<Linear_Programming_Task> Abc =
-		std::unique_ptr<Linear_Programming_Task>(parser.parse("netlib\\BLEND.SIF"));//BLEND,OSA-60,CRE-B,AFIRO
-	Agmon_Motzkin task(std::move(Abc));
-	for (size_t i = 1; i <= task.get_width(); ++i) {
-		Vector_Sparse lim;
-		lim.set(i, 1);
-		lim.set_length(task.get_width());
-		task.add_limitation(lim);
+		std::unique_ptr<Linear_Programming_Task>(parser.parse("netlib\\AFIRO.SIF"));//BLEND,OSA-60,CRE-B,AFIRO
+	ClpSimplex model;
+	auto& A = Abc->A;
+	auto& c = Abc->c;
+	std::vector<double> rowLower;
+	std::vector<double> objective;
+	for (size_t i = 1; i <= c->get_length(); ++i) {
+		objective.push_back(c->get(i));
 	}
-	std::vector<double> x(task.get_width(), 10);
-	task.set_x(x);
-	constexpr double E =  0.00000001;
-	constexpr size_t N = 100000;
-	auto [count, distance] = iterate(N, E, task);
-	std::cout << "Count = " << count << "\n";
-	std::cout << "Distance = " << distance << "\n";
-
-	/*auto get_biggest_violation = [&task]() {
-		double min = INT_MAX;
-		for (size_t j = 1; j <= task.get_height(); ++j) {
-			auto lim = task.check_limitation(j);
-			if (min > lim)
-				min = lim;
+	CoinPackedMatrix  matrix;
+	matrix.setDimensions(A->get_height(), A->get_width());
+	for (size_t r = 1; r <= A->get_height(); ++r) {
+		//std::cout << '\n';
+		rowLower.push_back(Abc->b->get(r));
+		for (size_t c = 1; c <= A->get_width(); ++c) {
+			matrix.modifyCoefficient(r - 1, c - 1, A->get(r, c));
+			//std::cout << A->get(r, c) << ":" << matrix.getCoefficient(r - 1, c - 1) << " ";
 		}
-		return min;
-	};*/
-	/*auto get_current_distance = [&task]() {
-		auto current_x = task.get_x();
-		task.next_x();
-		auto next_x = task.get_x();
-		return distance(current_x, next_x);
-	};*/
+	}
+	std::vector<double> rowUpper(rowLower.size(), COIN_DBL_MAX);
+	std::vector<double> colLower(objective.size(), 0.0);
+	std::vector<double> colUpper(objective.size(), COIN_DBL_MAX);
+	model.loadProblem(matrix, colLower.data(), colUpper.data(),
+		objective.data(), rowLower.data(), rowUpper.data());
+	model.setOptimizationDirection(1);
+	model.primal();
 
-	std::cout << "Biggest violation = " << task.get_biggest_violation() << " \n";
+	auto mat = model.matrix();
+	for (size_t r = 1; r <= A->get_height(); ++r) {
+		for (size_t c = 1; c <= A->get_width(); ++c) {
+			std::cout << mat->getCoefficient(r - 1, c - 1) << " ";
+		}
+		std::cout <<'|'<<model.getRowLower()[r-1]<< '\n';
+	}
+	
+
+	//omp_set_num_threads(4);
+	//MPS_Parser parser;
+	//std::unique_ptr<Linear_Programming_Task> Abc =
+	//	std::unique_ptr<Linear_Programming_Task>(parser.parse("netlib\\AFIRO.SIF"));//BLEND,OSA-60,CRE-B,AFIRO
+	//Agmon_Motzkin task(std::move(Abc));
+	//// x[i]>=0, i=1,...,n
+	//for (size_t i = 1; i <= task.get_width(); ++i) {
+	//	Vector_Sparse lim;
+	//	lim.set(i, 1);
+	//	lim.set_length(task.get_width());
+	//	task.add_limitation(lim);
+	//}
+	//std::vector<double> x(task.get_width(), 10);
+	//task.set_x(x);
+	//constexpr double E =  0/*.00001*/;
+	//constexpr size_t N = 10000000;
+	//auto [count, distance] = iterate(N, E, task);
+	//std::cout << "Count = " << count << "\n";
+	//std::cout << "Distance = " << distance << "\n";
+	//std::cout << "Biggest violation = " << task.get_biggest_violation() << " \n";
 
 	/*for (auto x : task.get_x())
 		std::cout << x << " ";*/
 
 	//	// f(x) -> min
-	double k1 = 1000;
-	double k2 = k1 / 2;
-	/*k1 = 580;
-	k2 = 570;*/
-	/*k1 = 1500;
-	k2 = 1000;*/
-	double upper_bound = task.calculate_criterion();
-	std::cout << "Upper bound = " << upper_bound << "\n\n";
-	upper_bound = task.calculate_criterion();
-	auto criterion = task.get_criterion();
-	task.add_limitation(criterion, upper_bound - k1);
+	//double k1 = 1000;
+	//double k2 = 0;
+	///*k1 = 580;
+	//k2 = 570;*/
+	///*k1 = 1500;
+	//k2 = 1000;*/
+	//double upper_bound = task.calculate_criterion();
+	//std::cout << "Upper bound = " << upper_bound << "\n\n";
+	//upper_bound = task.calculate_criterion();
+	//auto criterion = task.get_criterion();
+	//task.add_limitation(criterion, upper_bound - k1);
 
-	for (size_t i = 1; i <= criterion.get_length(); ++i) {
-		auto current = criterion.get(i);
-		if (current != 0.0)
-			criterion.set(i, -current);
-	}
-	task.add_limitation(criterion, -upper_bound + k2);
+	//for (size_t i = 1; i <= criterion.get_length(); ++i) {
+	//	auto current = criterion.get(i);
+	//	if (current != 0.0)
+	//		criterion.set(i, -current);
+	//}
+	//task.add_limitation(criterion, -upper_bound + k2);
 
-	do {
-		auto [count, distance] = iterate(N, E, task);
-		std::cout << "Count = " << count << "\n";
-		std::cout << "Distance = " << distance << "\n";
-		std::cout << "k1 = " << k1 << ", k2 = " << k2 << std::endl;
-		std::cout << "Criterion = " << task.calculate_criterion() << '\n';
-		auto biggest_violation = task.get_biggest_violation();
-		std::cout << "Biggest violation = " << biggest_violation << " \n";
-		if (distance <= E) {
-			k2 += (k1 - k2) / 2;
-		}
-		else {
-			k1 = k2;
-			k2 = k1 / 2;
-		}
-		size_t row = task.get_height();
-		task.set_b(row - 1, upper_bound - k1);
-		task.set_b(row, -upper_bound + k2);
-	} while (k1 - k2 > 1);
-	std::ofstream os("best_results.txt");
-	if (os.is_open()) {
-		auto [c, x] = task.get_best_results();
-		os << c << '\n';
-		for (auto& e : x)
-			os << e << ' ';
-	}
-	os.close();
+	//do {
+	//	auto [count, distance] = iterate(N, E, task);
+	//	std::cout << "Count = " << count << "\n";
+	//	std::cout << "Distance = " << distance << "\n";
+	//	std::cout << "k1 = " << k1 << ", k2 = " << k2 << std::endl;
+	//	std::cout << "Criterion = " << task.calculate_criterion() << '\n';
+	//	auto biggest_violation = task.get_biggest_violation();
+	//	std::cout << "Biggest violation = " << biggest_violation << " \n";
+	//	if (distance <= E) {
+	//		k2 += (k1 - k2) / 2;
+	//	}
+	//	else {
+	//		k1 = k2;
+	//		k2 = 0;
+	//	}
+	//	size_t row = task.get_height();
+	//	task.set_b(row - 1, upper_bound - k1);
+	//	task.set_b(row, -upper_bound + k2);
+	//} while (k1 - k2 > 1);
+	//std::ofstream os("best_results.txt");
+	//if (os.is_open()) {
+	//	auto [c, x] = task.get_best_results();
+	//	os << c << '\n';
+	//	for (auto& e : x)
+	//		os << e << ' ';
+	//}
+	//os.close();
 	system("pause");
 	return 0;
 }
